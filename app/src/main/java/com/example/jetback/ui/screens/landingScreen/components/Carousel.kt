715b5611d8eea34c6b91b4e5fc5b1811aab958c6
Shubham Singh <shubham.singh@mutualmobile.com>
2022-02-03 20:50:56 +0530

    Minor code refactoring

--- app/src/main/java/com/example/jetback/ui/screens/landingScreen/components/Carousel.kt
+++ app/src/main/java/com/example/jetback/ui/screens/landingScreen/components/Carousel.kt
@@ -39,6 +39,7 @@ import com.example.jetback.ui.utils.dpadFocusable
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.HorizontalPagerIndicator
+import com.google.accompanist.pager.PagerState
 import com.google.accompanist.pager.rememberPagerState
 import kotlinx.coroutines.launch
 
@@ -56,6 +57,15 @@ object Carousel {
     @OptIn(ExperimentalAnimationApi::class)
     val defaultTransition = fadeIn(animationSpec = tween(700)) +
         fadeIn() with fadeOut()
+
+    const val CarouselHeightFactor = 0.6f
+    const val CarouselButtonAlpha = 0.25f
+    const val CarouselBottomRowHPadding = 32
+    const val CarouselBottomRowVPadding = 8
+    const val VerticalGradientStart = 250f
+    const val VerticalGradientEnd = 700f
+    const val HorizontalGradientStart = -800f
+    const val HorizontalGradientEnd = 1000f
 }
 
 @OptIn(
@@ -67,135 +77,169 @@ fun Carousel(
     listOfMovies: List<Pair<Pair<Int, String>, String>> = ImageCard.listOfItems
 ) {
     val carouselState = rememberPagerState()
-    val coroutineScope = rememberCoroutineScope()
-    val buttonColors = ButtonDefaults.buttonColors(
-        backgroundColor = MaterialTheme.colors.primary.copy(alpha = 0.25f)
-    )
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight(0.6f),
+            .fillMaxHeight(Carousel.CarouselHeightFactor),
         contentAlignment = Alignment.BottomEnd,
     ) {
-        AnimatedContent(
-            targetState = carouselState.targetPage,
-            transitionSpec = { Carousel.defaultTransition },
-            modifier = Modifier
-                .fillMaxSize()
-                .dpadFocusable(
-                    isItemFocused = false,
-                    isCarousel = true,
-                    carouselActionLeft = {
-                        coroutineScope.launch {
-                            if (!carouselState.isScrollInProgress) {
-                                if (carouselState.currentPage > 0) {
-                                    carouselState.animateScrollToPage(carouselState.currentPage - 1)
-                                } else {
-                                    carouselState.animateScrollToPage(carouselState.pageCount - 1)
-                                }
-                            }
-                        }
-                    },
-                    carouselActionRight = {
-                        coroutineScope.launch {
-                            if (!carouselState.isScrollInProgress) {
-                                if (carouselState.currentPage < carouselState.pageCount - 1) {
-                                    carouselState.animateScrollToPage(carouselState.currentPage + 1)
-                                } else {
-                                    carouselState.animateScrollToPage(0)
-                                }
-                            }
-                        }
-                    }
-                )
-        ) { index ->
-            val imagePainter =
-                rememberImagePainter(data = listOfMovies[index].first.first) {
-                    crossfade(250)
-                    allowHardware(true)
-                }
-            Image(
-                imagePainter,
-                null,
-                modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.Crop
-            )
-        }
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(
-                    brush = Brush.verticalGradient(
-                        colors = listOf(
-                            Color.Transparent,
-                            Color.Black
-                        ),
-                        startY = 250f,
-                        endY = 700f
-                    )
-                )
-        ) {}
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(
-                    brush = Brush.horizontalGradient(
-                        colors = listOf(
-                            Color.Black,
-                            Color.Transparent,
-                        ),
-                        startX = -800f,
-                        endX = 1000f
-                    )
-                )
-        ) {}
+        CarouselImage(
+            carouselState = carouselState,
+            listOfMovies = listOfMovies
+        )
+        CarouselGradients()
+        // Only in order to populate the HorizontalPagerIndicator
         HorizontalPager(
             count = listOfMovies.size,
             modifier = Modifier.wrapContentSize(),
-            state = carouselState
-        ) {}
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 32.dp, vertical = 8.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.Bottom
-        ) {
-            Column(
-                verticalArrangement = Arrangement.Bottom
-            ) {
-                AnimatedContent(
-                    targetState = listOfMovies[carouselState.currentPage].first.second,
-                    transitionSpec = { Carousel.defaultTransition }
-                ) { movieTitle ->
-                    Text(
-                        movieTitle,
-                        style = Carousel.CarouselTypography.h1,
-                        modifier = Modifier.padding(8.dp)
-                    )
-                }
-                Button(
-                    onClick = {},
-                    shape = CircleShape,
-                    colors = buttonColors
-                ) {
-                    AnimatedContent(
-                        targetState = listOfMovies[carouselState.currentPage].second,
-                        transitionSpec = { Carousel.defaultTransition }
-                    ) { movieRent ->
-                        Text("Rent now for $movieRent")
+            state = carouselState,
+            content = {}
+        )
+        CarouselBottomRow(
+            carouselState = carouselState,
+            listOfMovies = listOfMovies
+        )
+    }
+}
+
+@OptIn(
+    ExperimentalPagerApi::class, ExperimentalAnimationApi::class,
+    androidx.compose.ui.ExperimentalComposeUiApi::class
+)
+@Composable
+private fun CarouselImage(
+    carouselState: PagerState,
+    listOfMovies: List<Pair<Pair<Int, String>, String>>
+) {
+    val coroutineScope = rememberCoroutineScope()
+    AnimatedContent(
+        targetState = carouselState.targetPage,
+        transitionSpec = { Carousel.defaultTransition },
+        modifier = Modifier
+            .fillMaxSize()
+            .dpadFocusable(
+                isItemFocused = false,
+                isCarousel = true,
+                carouselActionLeft = {
+                    coroutineScope.launch {
+                        if (!carouselState.isScrollInProgress) {
+                            if (carouselState.currentPage > 0) {
+                                carouselState.animateScrollToPage(carouselState.currentPage - 1)
+                            } else {
+                                carouselState.animateScrollToPage(carouselState.pageCount - 1)
+                            }
+                        }
+                    }
+                },
+                carouselActionRight = {
+                    coroutineScope.launch {
+                        if (!carouselState.isScrollInProgress) {
+                            if (carouselState.currentPage < carouselState.pageCount - 1) {
+                                carouselState.animateScrollToPage(carouselState.currentPage + 1)
+                            } else {
+                                carouselState.animateScrollToPage(0)
+                            }
+                        }
                     }
                 }
-            }
-            HorizontalPagerIndicator(
-                pagerState = carouselState,
-                modifier = Modifier
-                    .background(
-                        color = MaterialTheme.colors.onSurface.copy(alpha = 0.25f),
-                        shape = CircleShape
-                    )
-                    .padding(8.dp)
             )
-        }
+    ) { index ->
+        val imagePainter =
+            rememberImagePainter(data = listOfMovies[index].first.first)
+        Image(
+            imagePainter,
+            null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
     }
+}
+
+@OptIn(ExperimentalPagerApi::class, ExperimentalAnimationApi::class)
+@Composable
+private fun CarouselBottomRow(
+    carouselState: PagerState,
+    listOfMovies: List<Pair<Pair<Int, String>, String>>,
+) {
+    val buttonColors = ButtonDefaults.buttonColors(
+        backgroundColor = MaterialTheme.colors.primary.copy(alpha = Carousel.CarouselButtonAlpha)
+    )
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                horizontal = Carousel.CarouselBottomRowHPadding.dp,
+                vertical = Carousel.CarouselBottomRowVPadding.dp
+            ),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.Bottom
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            AnimatedContent(
+                targetState = listOfMovies[carouselState.currentPage].first.second,
+                transitionSpec = { Carousel.defaultTransition }
+            ) { movieTitle ->
+                Text(
+                    movieTitle,
+                    style = Carousel.CarouselTypography.h1,
+                    modifier = Modifier.padding(Carousel.CarouselBottomRowVPadding.dp)
+                )
+            }
+            Button(
+                onClick = {},
+                shape = CircleShape,
+                colors = buttonColors
+            ) {
+                AnimatedContent(
+                    targetState = listOfMovies[carouselState.currentPage].second,
+                    transitionSpec = { Carousel.defaultTransition }
+                ) { movieRent ->
+                    Text("Rent now for $movieRent")
+                }
+            }
+        }
+        HorizontalPagerIndicator(
+            pagerState = carouselState,
+            modifier = Modifier
+                .background(
+                    color = MaterialTheme.colors.onSurface.copy(alpha = Carousel.CarouselButtonAlpha),
+                    shape = CircleShape
+                )
+                .padding(8.dp)
+        )
+    }
+}
+
+@Composable
+private fun CarouselGradients() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color.Transparent,
+                        Color.Black
+                    ),
+                    startY = Carousel.VerticalGradientStart,
+                    endY = Carousel.VerticalGradientEnd
+                )
+            )
+    ) {}
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.horizontalGradient(
+                    colors = listOf(
+                        Color.Black,
+                        Color.Transparent,
+                    ),
+                    startX = Carousel.HorizontalGradientStart,
+                    endX = Carousel.HorizontalGradientEnd
+                )
+            )
+    ) {}
 }
